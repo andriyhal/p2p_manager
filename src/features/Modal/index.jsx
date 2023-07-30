3bb05950126b5652f3d96c4cd421f3a3e96370f0
andriih <andrii.halashevskyi@kenshoo.com>
2023-07-30 12:40:55 +0300

    Removed style css file

--- src/features/Modal/index.jsx
+++ src/features/Modal/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useForm} from 'react-hook-form';
 import {styled} from "@mui/system";
+import motionStyled from '@emotion/styled';
 import {Button} from "@mui/material";
 
 import {FormInputDropdown} from "./dropdown";
@@ -59,6 +60,35 @@ const ButtonContainer = styled(Button)({
     minWidth: "120px"
 })
 
+const ModalContainer = motionStyled.div`
+  .modal {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
+  }
+
+  .modal.open {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .modal_content {
+    background-color: #fff;
+    padding: 16px;
+    border-radius: 4px;
+  }
+
+  .modal_button {
+    margin-left: 8px;
+  }
+`;
+
 const Modal = ({isOpen, onClose, onAddTask}) => {
     const {
         control,
@@ -72,40 +102,42 @@ const Modal = ({isOpen, onClose, onAddTask}) => {
     };
 
     return (
-        <div className={`modal ${isOpen ? 'open' : ''}`}>
-            <div className="modal_content">
-                <h2>Добавить элемент</h2>
-                <FormContainer onSubmit={handleSubmit(onSubmit)}>
-                    <FormInputText
-                        label="Nickname: "
-                        name="nickName"
-                        control={control}
-                    />
-                    <FormInputDropdown
-                        name={"fiat"}
-                        control={control}
-                        label={"Fiat: "}
-                        options={FIAT_OPTIONS}
-                    />
-                    <FormInputDropdown
-                        name={"asset"}
-                        control={control}
-                        label={"Asset: "}
-                        options={ASSET_OPTIONS}
-                    />
-                    <FormInputDropdown
-                        name={"tradeType"}
-                        control={control}
-                        label={"Trade type: "}
-                        options={TRADE_TYPES_OPTIONS}
-                    />
-                    <ButtonContainer variant="contained" type="submit">Добавить</ButtonContainer>
-                    <ButtonContainer variant="contained" type="button" onClick={onClose}>
-                        Закрыть
-                    </ButtonContainer>
-                </FormContainer>
+        <ModalContainer>
+            <div className={`modal ${isOpen ? 'open' : ''}`}>
+                <div className="modal_content">
+                    <h2>Добавить элемент</h2>
+                    <FormContainer onSubmit={handleSubmit(onSubmit)}>
+                        <FormInputText
+                            label="Nickname: "
+                            name="nickName"
+                            control={control}
+                        />
+                        <FormInputDropdown
+                            name={"fiat"}
+                            control={control}
+                            label={"Fiat: "}
+                            options={FIAT_OPTIONS}
+                        />
+                        <FormInputDropdown
+                            name={"asset"}
+                            control={control}
+                            label={"Asset: "}
+                            options={ASSET_OPTIONS}
+                        />
+                        <FormInputDropdown
+                            name={"tradeType"}
+                            control={control}
+                            label={"Trade type: "}
+                            options={TRADE_TYPES_OPTIONS}
+                        />
+                        <ButtonContainer variant="contained" type="submit">Добавить</ButtonContainer>
+                        <ButtonContainer variant="contained" type="button" onClick={onClose}>
+                            Закрыть
+                        </ButtonContainer>
+                    </FormContainer>
+                </div>
             </div>
-        </div>
+        </ModalContainer>
     );
 };
 
