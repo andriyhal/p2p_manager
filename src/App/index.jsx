4cc1e36cfaebe1fa2a44eb50d70a7eb920bb49fa
acvid3 <acvid3@gmail.com>
2023-08-02 22:29:34 +0300

    fixed git

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { styled } from '@mui/system';
-import P2PTracker from '../p2p_tracker';
+import React from "react";
+import { styled } from "@mui/system";
+import P2PTracker from "../p2p_tracker";
 
-const AppContainer = styled('div')({
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
-    gridGap: '5px',
-    top: '100px',
-    left: '100px',
-    padding: '10px',
-    zIndex: '1000',
-    color: ' #fff',
-    background: '#333',
+const AppContainer = styled("div")({
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  gridGap: "5px",
+  top: "100px",
+  left: "100px",
+  padding: "10px",
+  zIndex: "1000",
+  color: " #fff",
+  background: "#333",
 });
 
 // create order buy/sell nav url
@@ -20,16 +20,11 @@ const AppContainer = styled('div')({
 // document.location.href = https://p2p.binance.com/uk-UA/myads?type=normal&code=default
 
 const App = () => {
-    const isLogin = document.location.origin === 'https://accounts.binance.com';
+  const isLogin = document.location.origin === "https://accounts.binance.com";
 
-<<<<<<< HEAD
-    console.log('777');
-
-=======
->>>>>>> 57b4bef13facb922a1994a192a6c297290157dd3
-    return (
-        <AppContainer>{isLogin ? <P2PTracker /> : <P2PTracker />}</AppContainer>
-    );
+  return (
+    <AppContainer>{isLogin ? <P2PTracker /> : <P2PTracker />}</AppContainer>
+  );
 };
 
 export default App;
--- src/p2p_tracker/index.jsx
+++ src/p2p_tracker/index.jsx
@@ -1,32 +1,16 @@
-import React from 'react';
-<<<<<<< HEAD
-import P2PTaskList from './P2PTaskList';
-import Button from '../../shared/ui/Button';
-import Modal from '../../features/Modal';
-import useP2PMonitor from './useP2PMonitor';
-=======
-import {P2PTasks} from '../p2p_tasks';
->>>>>>> 57b4bef13facb922a1994a192a6c297290157dd3
+import React from "react";
+import { P2PTasks } from "../p2p_tasks";
 
-import {P2PTrackerForm} from "../p2p_tracker_form";
+import { P2PTrackerForm } from "../p2p_tracker_form";
 import useP2PMonitor from "../shared/hooks/useP2PMonitor";
 
 export const P2PTracker = () => {
-    const {
-        isModalOpen,
-        tasks,
-        openModal,
-        closeModal,
-        handleDelete,
-        handleAddTaskAndParams,
-    } = useP2PMonitor();
+  const { tasks, handleDelete, handleAddTaskAndParams } = useP2PMonitor();
 
-    return (
-        <div>
-            <P2PTrackerForm
-                onAddTask={handleAddTaskAndParams}
-            />
-            <P2PTasks tasks={tasks} onDelete={handleDelete}/>
-        </div>
-    );
+  return (
+    <div>
+      <P2PTrackerForm onAddTask={handleAddTaskAndParams} />
+      <P2PTasks tasks={tasks} onDelete={handleDelete} />
+    </div>
+  );
 };
