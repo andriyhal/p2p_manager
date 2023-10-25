e154ec32ebda51728a01e6190ee0cdff6ef1afd2
andriih <andrii.halashevskyi@kenshoo.com>
2023-10-25 12:19:08 +0300

    Added styles

--- src/content_script/track-order/percentage_modifier/index.jsx
+++ src/content_script/track-order/percentage_modifier/index.jsx
@@ -1,88 +1,109 @@
-import React, { useState, useEffect } from 'react';
-import { Box, TextField } from '@mui/material';
-import { Controller } from 'react-hook-form';
-import { animationTransition } from '../TrashButton';
+import React, {useState, useEffect, useCallback} from 'react';
+import {Box, TextField} from '@mui/material';
+import {Controller} from 'react-hook-form';
+import {animationTransition} from '../TrashButton';
+import {styled} from "@mui/system";
 
 const textFieldStyle = {
-	width: '100px',
-	borderColor: '#1976D2',
-	color: '#1976D2'
+    width: '100px',
+    borderColor: '#1976D2',
+    color: '#1976D2'
 };
 
-const calculatePriceFromSpot = ({ p2pPrice, spotPrice }) =>
-	((p2pPrice / spotPrice) * 100).toFixed(2);
+const getFormattedPrice = (number) => new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+}).format(parseFloat(number))
 
-const PercentageModifier = ({ spotPrice, p2pPrice, onChange }) => {
-	const [percentage, setPercentage] = useState(
-		calculatePriceFromSpot({ spotPrice, p2pPrice })
-	);
+const calculatePercentageFromSpot = ({p2pPrice, spotPrice}) =>
+    ((p2pPrice / spotPrice) * 100);
 
-	const handlePercentageChange = event => {
-		const newPercentage = parseFloat(event.target.value).toFixed(2);
-		setPercentage(newPercentage);
-		event.target.value = ((spotPrice * newPercentage) / 100).toFixed(2);
-		onChange(event);
-	};
+const calculateP2pPrice = ({percentage, spotPrice}) =>
+    (spotPrice * (percentage / 100))
 
-	useEffect(() => {
-		setPercentage(calculatePriceFromSpot({ spotPrice, p2pPrice }));
-		onChange({
-			target: { value: calculatePriceFromSpot({ spotPrice, p2pPrice }) }
-		});
-	}, [spotPrice, p2pPrice]);
+//color: ${({isValid = true}) => isValid ? "green" : "red"}
+const FinalPriceLabel = styled("h5")`
+  font-size: 14px;
+  color: #1976D2;
+  font-weight: 400;
+`;
 
-	return (
-		<Box display='flex' flexDirection='row' alignItems='center' gap='10px'>
-			<h5 style={{ minWidth: '150px' }}>
-				{Number(spotPrice).toFixed(2)} * {percentage}% ={' '}
-				{(spotPrice * (percentage / 100)).toFixed(2)}
-			</h5>
 
-			<TextField
-				variant='outlined'
-				type='number'
-				size='small'
-				label='Percentage'
-				value={percentage}
-				onChange={handlePercentageChange}
-				style={textFieldStyle}
-				InputProps={{
-					inputProps: {
-						step: '0.10'
-					}
-				}}
-			/>
-		</Box>
-	);
+const PercentageModifier = ({spotPrice, p2pPrice, onChange}) => {
+    const [percentage, setPercentage] = useState(
+        calculatePercentageFromSpot({spotPrice, p2pPrice})
+    );
+
+    const handlePercentageChange = useCallback(event => {
+        const newPercentage = parseFloat(event.target.value);
+        setPercentage(newPercentage);
+        event.target.value = newPercentage;
+        onChange(event);
+    }, [onChange]);
+
+    useEffect(() => {
+        setPercentage(calculatePercentageFromSpot({spotPrice, p2pPrice}));
+        onChange({
+            target: {value: calculatePercentageFromSpot({spotPrice, p2pPrice})}
+        });
+    }, [spotPrice, p2pPrice]);
+
+    return (
+        <Box display='flex' flexDirection='row' alignItems='center' gap='10px'>
+            <FinalPriceLabel>
+                {getFormattedPrice(spotPrice)}
+                {" * "}
+                <b>{getFormattedPrice(percentage)}%</b>
+                {" = "}
+                {getFormattedPrice(calculateP2pPrice({percentage, spotPrice}))}
+            </FinalPriceLabel>
+
+            <TextField
+                variant='outlined'
+                type='number'
+                size='small'
+                label='Percentage'
+                value={percentage.toFixed(2)}
+                onChange={handlePercentageChange}
+                style={textFieldStyle}
+                InputProps={{
+                    inputProps: {
+                        step: '0.10'
+                    }
+                }}
+                sx={animationTransition}
+            />
+        </Box>
+    );
 };
 
 export const FormPercentageModifier = ({
-	label,
-	name,
-	control,
-	defaultValue,
-	spotPrice,
-	p2pPrice
-}) => {
-	return (
-		<Controller
-			name={name}
-			control={control}
-			defaultValue={defaultValue}
-			render={({ field: { onChange, value }, fieldState: { error } }) => (
-				<PercentageModifier
-					spotPrice={spotPrice}
-					p2pPrice={p2pPrice}
-					helperText={error ? error.message : null}
-					size='small'
-					error={!!error}
-					onChange={onChange}
-					fullWidth
-					label={label}
-					variant='outlined'
-					sx={animationTransition}
-				/>
-			)}
-		/>
-	);
+                                           label,
+                                           name,
+                                           control,
+                                           defaultValue,
+                                           spotPrice,
+                                           p2pPrice
+                                       }) => {
+    return (
+        <Controller
+            name={name}
+            control={control}
+            defaultValue={defaultValue}
+            render={({field: {onChange}, fieldState: {error}}) => (
+                <PercentageModifier
+                    spotPrice={spotPrice}
+                    p2pPrice={p2pPrice}
+                    helperText={error ? error.message : null}
+                    size='small'
+                    error={!!error}
+                    onChange={onChange}
+                    fullWidth
+                    label={label}
+                    variant='outlined'
+                    sx={animationTransition}
+                />
+            )}
+        />
+    );
 };
